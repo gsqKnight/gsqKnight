090bae7445ea7879c507f40052679ef5883c70af
老郭 <528092955@qq.com>
2022-09-26 17:37:37 +0800

    项目经理操作成功

--- src/main/java/comm/beijing/one.java
+++ src/main/java/comm/beijing/one.java
@@ -3,5 +3,6 @@ package comm.beijing;
 public class one {
     public static void main(String[] args) {
         System.out.println("22222222222222222222222222springWeb");
+        System.out.println("adsasdas");
     }
 }
--- src/main/java/comm/beijing/tow.java
+++ src/main/java/comm/beijing/tow.java
@@ -3,5 +3,6 @@ package comm.beijing;
 public class tow {
     public static void main(String[] args) {
         System.out.println("222222打赏阿萨打赏2222222");
+        System.out.println("huibiu");;
     }
 }
